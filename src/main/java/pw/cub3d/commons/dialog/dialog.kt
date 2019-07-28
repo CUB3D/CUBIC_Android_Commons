3be8e912cc8ab0ca09c7250fce00f3ad7437f11e
CUB3D <callumthom11@gmail.com>
2019-07-28 02:14:58 +0100

    Added dialog

---
+++ src/main/java/pw/cub3d/commons/dialog/dialog.kt
@@ -0,0 +1,55 @@
+package pw.cub3d.commons.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.view.View
+import android.view.Window
+import android.widget.ProgressBar
+import android.widget.TextView
+import pw.cub3d.commons.R
+
+class DialogBuilder(val ctx: Context) {
+    val dialog: pw.cub3d.commons.dialog.Dialog = pw.cub3d.commons.dialog.Dialog(ctx)
+
+    init {
+        withText("")
+        withTitle("")
+    }
+
+    fun withTitle(title: String) {
+        dialog.title.text = title
+    }
+
+    fun withText(text: String) {
+        dialog.contentText.text = text
+    }
+
+    fun withSpinner() {
+        dialog.spinner.visibility = View.VISIBLE
+    }
+
+    fun loadIndefinitely() {
+        dialog.setCancelable(false)
+        withSpinner()
+    }
+
+    fun show(): Dialog {
+        dialog.show()
+        return dialog
+    }
+}
+
+class Dialog(val ctx: Context) : Dialog(ctx) {
+
+    val title: TextView
+    val contentText: TextView
+    val spinner: ProgressBar
+
+    init {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog)
+        title = findViewById(R.id.dialog_Title)
+        contentText = findViewById(R.id.dialog_Content)
+        spinner = findViewById(R.id.dialog_spinner)
+    }
+}
