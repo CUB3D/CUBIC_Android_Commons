61329fc30842c73b1ee450c1788c9e88a51f08e6
CUB3D <callumthom11@gmail.com>
2019-09-04 15:50:08 +0100

    Updates

--- src/main/java/pw/cub3d/commons/logging/Log.kt
+++ src/main/java/pw/cub3d/commons/logging/Log.kt
@@ -1,7 +1,12 @@
 package pw.cub3d.commons.logging
 
-object Log: Logger("Test") {
+
+object Log: Logger("BaseLogger") {
     fun getLogHistory(): List<String> {
         return logHistory
     }
 }
+
+fun Any.getLogger() = Logger(this.javaClass.simpleName)
+
+fun Any.debug(msg: String) = getLogger().d(msg)
